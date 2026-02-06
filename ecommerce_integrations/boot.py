69ab19f6391cabb7f239679accff1d871b3606eb
VIKAS RAJORA <v.vikas.rajora@Gmail.com>
2026-02-07 01:27:35 +0530

    Update boot.py

--- ecommerce_integrations/boot.py
+++ ecommerce_integrations/boot.py
@@ -1,8 +1,14 @@
-from ecommerce_integrations.shopify.constants import OLD_SETTINGS_DOCTYPE
+try:
+	from ecommerce_integrations.shopify.constants import OLD_SETTINGS_DOCTYPE
+except ModuleNotFoundError:
+	OLD_SETTINGS_DOCTYPE = None
 
 
 def boot_session(bootinfo):
 	"""Don't show old doctypes after enabling new ones."""
+	if not OLD_SETTINGS_DOCTYPE:
+		return
+
 	try:
 		bootinfo.single_types.remove(OLD_SETTINGS_DOCTYPE)
 	except ValueError:
