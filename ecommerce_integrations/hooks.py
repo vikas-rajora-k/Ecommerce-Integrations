a1d25c8142efc8d67d5ee308601a86bd279e4da5
vikas-rajora-k <vikas.rajora@karban.in>
2026-02-06 12:20:44 +0530

    Change required_apps from 'frappe/erpnext' to 'erpnext'

--- ecommerce_integrations/hooks.py
+++ ecommerce_integrations/hooks.py
@@ -8,7 +8,7 @@ app_icon = "octicon octicon-file-directory"
 app_color = "grey"
 app_email = "developers@frappe.io"
 app_license = "GNU GPL v3.0"
-required_apps = ["frappe/erpnext"]
+required_apps = ["erpnext"]
 
 # Includes in <head>
 # ------------------
